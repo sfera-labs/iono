9738753275edef83b29d00ac22d5e44b84100f1b
Giampiero <giampiero.baggiani@gmail.com>
2015-01-14 15:13:31 +0100

    removed unused code

--- IonoUDP/examples/SimpleUDP/SimpleUDP.ino
+++ IonoUDP/examples/SimpleUDP/SimpleUDP.ino
@@ -13,25 +13,9 @@ unsigned int port = 7878;
 
 EthernetUDP Udp;
 
-//char macStr[18];
-
 void setup() {
   Ethernet.begin(mac, ip, gateway, subnet);
   Udp.begin(port);
-
-//  for (int i = 0; i < 6; i++) {
-//    int j = i * 2;
-//    if (i != 0) {
-//      j += i - 1;
-//      macStr[j++] = ':';
-//    }      
-//    macStr[j] = (mac[i] >> 4) + 0x30;
-//    if (macStr[j] > 0x39) macStr[j] +=7;
-//    macStr[j + 1] = (mac[i] & 0x0f) + 0x30;
-//    if (macStr[j + 1] > 0x39) macStr[j + 1] +=7;
-//  }
-//  macStr[17] = '\0';
-
   IonoUDP.begin("myIono", Udp, port, 50, 0.1);
 }
 
